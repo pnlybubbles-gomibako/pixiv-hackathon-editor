15cd54cba26aea1c7d35a6ee01ac374d912b30d9
pnlybubbles <pnlybubbles@gmail.com>
2016-10-23 13:19:10 +0900

    feat: add feature

--- src/main.js
+++ src/main.js
@@ -7,10 +7,10 @@ new Vue({
     step: 0,
     buttons: {
       left: {
-        label: 'Back',
+        label: 'BACK',
       },
       right: {
-        label: 'Next',
+        label: 'NEXT',
       },
     },
   },
@@ -49,7 +49,8 @@ new Vue({
       this.$refs.page1.templates = res[0].templateConfig.templates;
       this.$refs.page2.works = res[0].works;
       this.id = res[1].id;
-      this.$els.iframe.contentDocument.location.replace(`https://mmapi.grimoire.gl/api/museum/${res[1].id}`);
+      this.$els.iframe.src = `https://mmapi.grimoire.gl/api/museum/${res[1].id}`;
+      // this.$els.iframe.contentDocument.location.replace(`https://mmapi.grimoire.gl/api/museum/${res[1].id}`);
     }).catch((e) => {
       console.error(e);
     });
@@ -94,7 +95,7 @@ new Vue({
           params: JSON.stringify(json),
         }));
       }).then((res) => {
-        this.$els.iframe.contentDocument.location.reload();
+        this.$els.iframe.src = this.$els.iframe.src;
       });
     },
   }
